08e498afc93f59df0454f56f9af68f6a97b1e265
Heitor <mafaldoheitor@gmail.com>
2026-04-24 17:36:47 -0300

    Consertar erro no main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Button from "./components/Button"
+import Button from "./components/Button.jsx"
+import Header from "./components/Header.jsx"
 
 function App() {
 
   return (
     <>
+    <Header></Header>
      <Button buttonTitle={"Converter"}></Button>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+
+function Header(){
+    return (
+        <>
+            <header>
+                <h1>Converter de binário para decimal </h1>
+                <p>Desenvolvido em React JS + Javascript</p>
+            </header>
+        </>
+    )
+}
+
+export default Header
